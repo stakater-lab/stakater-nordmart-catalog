5a4f05a4ae1c265215b031343b64bd4097fec7b2
Muhammad Hamza Zaib <hamza@aurorasolutions.io>
2019-06-11 18:57:32 +0500

    Add Filename to products

--- src/main/java/com/stakater/nordmart/catalog/domain/Product.java
+++ src/main/java/com/stakater/nordmart/catalog/domain/Product.java
@@ -1,7 +1,6 @@
 package com.stakater.nordmart.catalog.domain;
 
 import java.io.Serializable;
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -9,54 +8,77 @@ import javax.persistence.UniqueConstraint;
 
 @Entity
 @Table(name = "PRODUCT", uniqueConstraints = @UniqueConstraint(columnNames = "itemId"))
-public class Product implements Serializable {
+public class Product implements Serializable
+{
 
-	@Id
-	private String itemId;
+    @Id
+    private String itemId;
 
-	private String name;
+    private String name;
 
-	private String description;
+    private String description;
 
-	private double price;
+    private String fileName;
 
-	public Product() {
-	}
+    private double price;
 
-	public String getItemId() {
-		return itemId;
-	}
+    public Product()
+    {
+    }
 
-	public void setItemId(String itemId) {
-		this.itemId = itemId;
-	}
+    public String getItemId()
+    {
+        return itemId;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setItemId(String itemId)
+    {
+        this.itemId = itemId;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName()
+    {
+        return name;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public void setName(String name)
+    {
+        this.name = name;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public String getDescription()
+    {
+        return description;
+    }
 
-	public double getPrice() {
-		return price;
-	}
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
 
-	public void setPrice(double price) {
-		this.price = price;
-	}
+    public String getFileName()
+    {
+        return fileName;
+    }
 
-	@Override
-	public String toString() {
-		return "Product [itemId=" + itemId + ", name=" + name + ", price=" + price + "]";
-	}
+    public void setFileName(String fileName)
+    {
+        this.fileName = fileName;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public void setPrice(double price)
+    {
+        this.price = price;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Product [itemId=" + itemId + ", name=" + name + ", price=" + price + "]";
+    }
 }
