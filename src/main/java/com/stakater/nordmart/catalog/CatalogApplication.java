3f36ad889b1609891b0a6fced849aef07fa1572a
Ahmed Waleed Malik <ahmed.waleed@aurorasolutions.io>
2019-11-13 17:58:57 +0500

    add-mysql use mysql instead of in-memory DB

--- src/main/java/com/stakater/nordmart/catalog/CatalogApplication.java
+++ src/main/java/com/stakater/nordmart/catalog/CatalogApplication.java
@@ -2,11 +2,14 @@ package com.stakater.nordmart.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EntityScan
+@EnableJpaRepositories
 @SpringBootApplication
 public class CatalogApplication
 {
-
     public static void main(String[] args) {
         SpringApplication.run(CatalogApplication.class, args);
     }
--- src/main/java/com/stakater/nordmart/catalog/domain/Product.java
+++ src/main/java/com/stakater/nordmart/catalog/domain/Product.java
@@ -2,16 +2,23 @@ package com.stakater.nordmart.catalog.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "PRODUCT", uniqueConstraints = @UniqueConstraint(columnNames = "itemId"))
 public class Product implements Serializable {
 
+	// Specify column length and size to avoid SQLSyntaxErrorException: Specified key was too long; max key length is 1000 bytes
 	@Id
+	@NotNull
+	@Size(min = 0, max = 50)
+	@Column(length = 50)
 	private String itemId;
 
 	private String name;
