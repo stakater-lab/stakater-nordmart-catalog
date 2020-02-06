4a5c38b989cf17234e21d8bc2fd13e9e37e8785e
Ahmed Waleed Malik <ahmedwaleedmalik@gmail.com>
2020-02-06 12:07:19 +0500

    Add traced annotation to new rest methods

--- src/main/java/com/stakater/nordmart/catalog/controller/CatalogController.java
+++ src/main/java/com/stakater/nordmart/catalog/controller/CatalogController.java
@@ -1,7 +1,5 @@
 package com.stakater.nordmart.catalog.controller;
 
-import com.stakater.nordmart.catalog.common.IstioHeaders;
-import com.stakater.nordmart.catalog.common.Utils;
 import com.stakater.nordmart.catalog.domain.Product;
 import com.stakater.nordmart.catalog.dto.ProductDto;
 import com.stakater.nordmart.catalog.exception.NotFoundException;
@@ -19,7 +17,15 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 import java.util.Optional;
@@ -73,6 +79,7 @@ public class CatalogController {
         return productList;
     }
 
+    @Traced
     @ApiOperation(value = "Saves product.",
             consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
@@ -99,6 +106,7 @@ public class CatalogController {
         }
     }
 
+    @Traced
     @ApiOperation(value = "Gets product by provided itemId. Returns 404 status if such product  was not found.",
             consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
     @GetMapping(path = "/{itemId}", produces = APPLICATION_JSON_VALUE)
--- src/test/java/com/stakater/nordmart/BaseTest.java
+++ src/test/java/com/stakater/nordmart/BaseTest.java
@@ -4,8 +4,6 @@ package com.stakater.nordmart;
 import com.stakater.nordmart.catalog.CatalogApplication;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
